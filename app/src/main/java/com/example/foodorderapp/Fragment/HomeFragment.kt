46f8c55dcea848a39d829081af711f3009cab02d
Sanjoy Kumar Roy <roysanjoy.cse@gmail.com>
2024-10-07 22:52:31 +0600

    Adding popular food items finished

--- app/src/main/java/com/example/foodorderapp/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/foodorderapp/Fragment/HomeFragment.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.interfaces.ItemClickListener
 import com.denzcoskun.imageslider.models.SlideModel
 
 import com.example.foodorderapp.R
+import com.example.foodorderapp.adaptar.PopularAdaptar
 import com.example.foodorderapp.databinding.FragmentHomeBinding
 
 
@@ -60,6 +62,15 @@ class HomeFragment : Fragment() {
                 Toast.makeText(requireContext(),itemMessage,Toast.LENGTH_SHORT).show()
             }
         })
+
+        val foodName = listOf("Burger","Sandwich","momo","ice-cream")
+        val price = listOf("$5","$7","$8","$10")
+        val popularFoodImages = listOf(R.drawable.menu1, R.drawable.menu2, R.drawable.menu3,R.drawable.menu4)
+
+        val adapter = PopularAdaptar(foodName, price,popularFoodImages)
+        binding.popularRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.popularRecyclerView.adapter = adapter
+
     }
 
     companion object {
