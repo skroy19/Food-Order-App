badbd9558950c8fd1a648c923ae303149534f996
Sanjoy Kumar Roy <roysanjoy.cse@gmail.com>
2024-10-07 20:03:49 +0600

    bottom navigation and frament work done

--- app/src/main/java/com/example/foodorderapp/MainActivity.kt
+++ app/src/main/java/com/example/foodorderapp/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +15,9 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-
+        var NavController = findNavController(R.id.fragmentContainerView2)
+        var bottomnav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomnav.setupWithNavController(NavController)
 
 
 
